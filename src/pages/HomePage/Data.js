98acb4186bb880b4e34f40091e54e31df390577b
Pedro <pedrodaserenidade@gmail.com>
2021-06-21 15:45:49 -0300

    first version

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,63 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Our team of unique especialist can help you acheive your business goals.', 
+    headline: 'Lead Generation Specialist for Online Businesses', 
+    lightText:  true, 
+    topLine: 'Marketing Agency',
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Image', 
+    start: ''
+}
+
+export const homeObjTwo = {
+    lightBg: true,
+    primary: false, 
+    imgStart: 'start', 
+    lightTopLine: false, 
+    lightTextDesc: false, 
+    buttonLabel: 'Other Depoiments', 
+    description: 'ULTRA provides a team of expense-saving experts to maximize your revenue.', 
+    headline: 'Ultra helped me increase my revenue by over 3X in less than 3 months', 
+    lightText:  false, 
+    topLine: 'Sarah Jones',
+    img: require('../../images/profile.jpg').default,
+    alt: 'Image', 
+    start: 'true'
+}
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'See our dashboard', 
+    description: 'Our tools provide reports with efficient, detailed and real-time dashboards. nothing will escape your eyes.', 
+    headline: 'Extremely quick onboarding process', 
+    lightText:  true, 
+    topLine: '',
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Image', 
+    start: ''
+}
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true, 
+    imgStart: 'start', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Security Guidelines', 
+    description: 'Our servers have ostensible security designed by the greatest professionals in digital security on the market. Daily updated passwords and end-to-end encryption. Your data in a digital armored vault.', 
+    headline: 'All your data stored on our secure server', 
+    lightText:  true, 
+    topLine: '',
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Image', 
+    start: 'true'
+}
